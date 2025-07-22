c4525f91fa849f9f959e10938aeaf2fbc8fd6665
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2025-07-22 15:49:57 -0300

    sssssssssssssssssssssssss

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
@@ -7,23 +7,21 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-
-function diminuiTamanho(){
-if(tamanhoSenha > 1){
-//tamanhoSenha = tamanhoSenha - 1;
-tamanhoSenha--;
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-numeroSenha.textContent = tamanhoSenha;
-geraSenha();
-}
-
-function aumentaTamanho(){
-if(tamanhoSenha < 20) {
-//tamanhoSenha = tamanhoSenha + 1;
-tamanhoSenha++;
-}
-numeroSenha.textContent = tamanhoSenha;   
-geraSenha();
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 const campoSenha = document.querySelector('#campo-senha');
@@ -31,20 +29,13 @@ const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 geraSenha();
 
-function geraSenha(){
+function geraSenha() {
     let senha = '';
-
-    for(let i = 0; i , tamanhoSenha; i++){
-
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
-   
-
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
     }
-campoSenha.value = senha;
-    
+    campoSenha.value = senha;
 }
-
-
 
