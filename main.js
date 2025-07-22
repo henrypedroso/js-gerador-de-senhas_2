221ef24e5e709b002923935175aba23572c72f16
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2025-07-22 15:32:21 -0300

    sssssssssssssssssssss

--- main.js
+++ main.js
@@ -31,9 +31,16 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 geraSenha();
 
 function geraSenha(){
+
+    for(let i = 0; i , tamanhoSenha; i++){
+
     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
     console.log(letrasMaiusculas[numeroAleatorio]);
+
+    }
+
+    
 }
 
 campoSenha.value = letrasMaiusculas;
