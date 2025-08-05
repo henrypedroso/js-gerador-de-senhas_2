eaa77b262c9daecc7142128320ec73171a0fc1c6
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2025-08-05 07:30:44 -0300

    sssssssssssssssssssss

--- main.js
+++ main.js
@@ -27,14 +27,37 @@ function aumentaTamanho() {
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
-console.log(checkbox [0].checked);
+for(i = 0; < checkbox.length; i++){
+    checkbox[i].onclick = geraSenha;
+}
+
+
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
+const simbolos = '!@%*?';
 geraSenha();
 
 function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+
+     if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+
+
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
